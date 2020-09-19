7dc41f75cd01fb6af2508f5e6a180390aafb39ee
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-19 15:32:00 +0000

    Write a program that prints the numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+include <stdio.h>
+
+/**
+ * main - Function to print numbers from 00 until 99 with commas using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	int d;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (d = '0'; d <= '9' ; d++)
+		{
+			putchar (c);
+			putchar (d);
+			if (c < '9' || d < '9')
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
