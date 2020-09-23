9febeb69c9730741bc3183af3008ebadf068893d
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-23 03:50:11 +0000

    In most projects, we often give you only one main file to test

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/*Function that give the sign of any number*/
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */#ifndef HOLBERTON_H
+
