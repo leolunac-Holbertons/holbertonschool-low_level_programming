8593dbb232f915acb5a293af1aa2e3db2afe0129
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-23 00:23:30 +0000

    Write a program that prints Holberton, followed by a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - Prints Holbertons
+ * main - Print Holbertons with _putchar.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
-
