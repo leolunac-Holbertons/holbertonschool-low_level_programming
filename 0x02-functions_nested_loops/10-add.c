8bd4b6b0158eb5b4c2251a5a30f049d2de12ca02
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-23 02:48:53 +0000

    Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+include "holberton.h"
+
+/**
+ * add - Sum of two numbers.
+ * @x: 1er number to add.
+ * @y: 2do number to add.
+ *
+ * Return: Value of "last" digit.
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
