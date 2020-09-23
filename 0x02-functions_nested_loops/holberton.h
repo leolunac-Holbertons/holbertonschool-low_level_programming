8e29c3ee97f41d24b740938fa458a3dbbd4a3266
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-23 01:43:34 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,34 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*Putchar holberton function*/
+int _putchar(char c);
+
+/*Print alphabet lowercase*/
+void print_alphabet(void);
+
+/*Print alphabet 10 times*/
+void print_alphabet_x10(void);
+
+/*Checks for lowercase character*/
+int _islower(int c);
+
+/*Checks for alphabet character*/
+int _isalpha(int c);
+
+/*Give the sign of a number*/
+int print_sign(int n);
+
+/*Computes the absolute value of an integer*/
+int _abs(int);
+
+/*Function to get the last digit of any number*/
+int print_last_digit(int);
+
+/*Function to count every minute until 24 minutes*/
+void jack_bauer(void);
+
+/*Function to add two numbers*/
+int add(int, int);
+
+#endif /* _HOLBERTON_H_ */
