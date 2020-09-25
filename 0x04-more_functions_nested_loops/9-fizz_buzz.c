2d9a2b4cd62dc3c58ab83ef83b9d60f87b469063
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-25 04:52:13 +0000

    The Fizz-Buzz test is an interview question designed to help filter out the 99.5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,7 +21,14 @@ int main(void)
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz ");
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
 		}
 		else
 		{
