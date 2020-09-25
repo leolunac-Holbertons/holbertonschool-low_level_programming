1805dd2d036493a06f481d81eaa7e997b33f74e5
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-25 04:14:29 +0000

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,37 @@
+fndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Function that prints characters*/
+int _putchar(char c);
+
+/*Function that reconize uppercase characters*/
+int _isupper(int c);
+
+/*Function that checks digits*/
+int _isdigit(int c);
+
+/*Function to multiplie two integers*/
+int mul(int a, int b);
+
+/*Function to print number from 0 to 9*/
+void print_numbers(void);
+
+/*Function to print nnumber 0 to 9.Do not print 2 and 4*/
+void print_most_numbers(void);
+
+/*Function that prints 10 times the numbers, from 0 to 14*/
+void more_numbers(void);
+
+/*Funtion to print line*/
+void print_line(int n);
+
+/*function that draws a diagonal line on the terminal*/
+void print_diagonal(int n);
+
+/*Function to print a square*/
+void print_square(int size);
+
+/*Function to print a triangle*/
+void print_triangle(int size);
+
+#endif /* HOLBERTON_H */
