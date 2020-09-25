f533fe6b5e582af8c03bcac106cb92f971ddf886
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-25 19:14:44 +0000

    n most projects, we often give you only one main file to test with.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - Prints if integer is positive or negative
- *
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
 
-	i = 0;
-	positive_or_negative(i);
+	i = 98;
+	 positive_or_negative(i);
+
 	return (0);
 }
