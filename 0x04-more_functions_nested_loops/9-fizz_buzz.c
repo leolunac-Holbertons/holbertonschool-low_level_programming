184bea414265c4b43a1d403f34f2632e2cf3403a
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-25 04:48:08 +0000

    The Fizz-Buzz test is an interview question designed to help filter out the 99.5%

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz Buzz test.
+ *
+ * Return: Return 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1 ; num <= 100 ; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
