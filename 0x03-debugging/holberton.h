3e0c492ae5a1bdba7d3efde6ba92647746930ae0
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-25 18:56:33 +0000

    This program prints the largest of three integers.

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,5 +6,13 @@
 /*Function that give the sign of any number*/
 void positive_or_negative(int i);
 
-#endif /* HOLBERTON_H */
+/*Function that give the largest number of three*/
+int largest_number(int a, int b, int c);
 
+/*Function that convert day*/
+int convert_day(int month, int day);
+
+/*Function that print remaining*/
+void print_remaining_days(int month, int day, int year);
+
+#endif /* HOLBERTON_H */
