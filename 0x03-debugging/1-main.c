f8c3fef65956a56eea3eaee2e66e8757d29c3bca
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-25 20:09:16 +0000

    Copy this main file. Comment out (dont delete it!

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
+ * @\\o/\n: Funny face
  *
  * Return: 0
  */
@@ -9,11 +11,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
-/**
- * Here begins the loop
- *
+/*
+ * while - Here begins the loop
  *while (i < 10)
  *{
  *      putchar(i);
