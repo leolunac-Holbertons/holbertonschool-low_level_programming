3034f6549ff35878bf9e4dd822b4d1ad6974c97f
leolunac-Holbertons <2176@holbertonschool.com>
2020-09-29 09:59:36 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Function that prints characters*/
+int _putchar(char c);
+
+/*Function to update a value by pointer*/
+void reset_to_98(int *n);
+
+/*Function to swap the values of two integers*/
+void swap_int(int *a, int *b);
+
+/*Function that returns the length of a string*/
+int _strlen(char *s);
+
+/*Function that prints a string*/
+void _puts(char *str);
+
+/*Function that prints a string, in reverse*/
+void print_rev(char *s);
+
+/*Function that reverses a string*/
+void rev_string(char *s);
+
+#endif /* HOLBERTON_H */
