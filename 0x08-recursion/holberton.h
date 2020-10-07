fd51facefc852f9cbfe94bf5cd98a9f9116e7c3f
leolunac-Holbertons <2176@holbertonschool.com>
2020-10-07 21:15:56 +0000

    0x08. C - Recursion

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Function to print a character*/
+int _putchar(char c);
+
+/*Function that prints a string*/
+void _puts_recursion(char *s);
+
+/*Function to print a string in reverse*/
+void _print_rev_recursion(char *s);
+
+/*Function that returns the length of a string*/
+int _strlen_recursion(char *s);
+
+/*Function that return factorial number*/
+int factorial(int n);
+
+/*Function that returns the value of x raised to the power of y*/
+int _pow_recursion(int x, int y);
+
+#endif /* HOLBERTON_H */
+
