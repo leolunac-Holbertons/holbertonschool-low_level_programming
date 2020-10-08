cc68bc2aad239f1331298e125445f4c473b7533a
leolunac-Holbertons <2176@holbertonschool.com>
2020-10-08 01:37:13 +0000

    0x08. C - Recursion

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -19,5 +19,8 @@ int factorial(int n);
 /*Function that returns the value of x raised to the power of y*/
 int _pow_recursion(int x, int y);
 
+/*Function that returns the natural square root of a number*/
+int _sqrt_recursion(int n);
+
 #endif /* HOLBERTON_H */
 
