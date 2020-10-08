deea57f6358a1987fed549b82930b6275ce1e170
leolunac-Holbertons <2176@holbertonschool.com>
2020-10-08 22:57:01 +0000

    Write a function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -12,19 +12,19 @@
  */
 int pali(int i, int len, char *s)
 {
-  if (i == len - 1)
-    {
-      return (1);
-    }
-  else if (s[i] != s[len - 1])
-    {
-      return (0);
-    }
-  else if (i < len - 1)
-    {
-      return (pali(i + 1, len - 1, s));
-    }
-  return (1);
+	if (i == len - 1)
+	{
+		return (1);
+	}
+	else if (s[i] != s[len - 1])
+	{
+		return (0);
+	}
+	else if (i < len - 1)
+	{
+		return (pali(i + 1, len - 1, s));
+	}
+	return (1);
 }
 /**
  * lenght - Function for calculate lenght of the string.
@@ -36,11 +36,11 @@ int pali(int i, int len, char *s)
  */
 int lenght(char *s)
 {
-  if (*s != '\0')
-    {
-      return (1 + lenght(s + 1));
-    }
-  return (0);
+	if (*s != '\0')
+	{
+		return (1 + lenght(s + 1));
+	}
+	return (0);
 }
 /**
  * is_palindrome - Function to evaluate is a palindrome.
@@ -52,7 +52,7 @@ int lenght(char *s)
  */
 int is_palindrome(char *s)
 {
-  int len = lenght(s);
+	int len = lenght(s);
 
-  return (pali(0, len, s));
+	return (pali(0, len, s));
 }
