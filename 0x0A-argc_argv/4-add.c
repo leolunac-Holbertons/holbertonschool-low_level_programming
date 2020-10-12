2b2591b8ca663b88e11fce746967b33893f8d6fc
leolunac-Holbertons <2176@holbertonschool.com>
2020-10-12 00:54:13 +0000

    Write a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Program that add two positive numbers
+* @argc: Number of command lines arguments.
+* @argv: Character of arrays.
+*
+* Return: 0 when bo number is passed or 1 for error, when are not digits
+*/
+int main(int argc, char *argv[])
+{
+	int i, j;
+	unsigned long int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; *(argv[i] + j) != '\0'; j++)
+			{
+				if (argv[i][j] >= '0' && argv[i][j] <= '9')
+					continue;
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%lu\n", sum);
+	}
+	return (0);
+}
+
