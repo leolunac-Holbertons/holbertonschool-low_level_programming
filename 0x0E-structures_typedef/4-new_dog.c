8732792bd08aba1f17e93250ae2540534ad79302
leolunac-Holbertons <2176@holbertonschool.com>
2020-10-20 20:05:34 +0000

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,27 +1,72 @@
-#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "dog.h"
+
 /**
-* new_dog - new cute dog.
+* _strdup - Function returns a pointer to a newly allocated space in memory.
+* @str: string to copy.
 *
-*@name: name of dog.
-*@age: age of dog.
-*@owner: owner of dog.
+* Return: pointer to the array. NULL is size is 0 or fails.
+*/
+char *_strdup(char *str)
+{
+	unsigned int j = 0;
+	unsigned int i;
+	char *ptr;
+
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	ptr = malloc(sizeof(char) * (j + 1));
+	if (ptr != NULL)
+	{
+		for (i = 0; i <= j; i++)
+		{
+			ptr[i] = str[i];
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (ptr);
+}
+
+/**
+* new_dog - Function that creates a new dog.
+* @name: String of the dog's name
+* @age: integer for the age of the dog
+* @owner: string of the owner's name
 *
-* Return: Always 0.
-	  */
+* Return: a pointer to a struct.
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *x;
+	dog_t *new;
 
-	x = malloc(sizeof(dog_t));
-
-	if (x == NULL)
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+	{
 		return (NULL);
-
-	x->name = name;
-	x->age = age;
-	x->owner = owner;
-
-	return (x);
+	}
+	new->name = _strdup(name);
+	if (new->name == NULL)
+	{
+		free(new->name);
+		return (NULL);
+	}
+	new->owner = _strdup(owner);
+	if (new->owner == NULL)
+	{
+		free(new);
+		free(new->name);
+		return (NULL);
+	}
+	new->age = age;
+	return (new);
 }
