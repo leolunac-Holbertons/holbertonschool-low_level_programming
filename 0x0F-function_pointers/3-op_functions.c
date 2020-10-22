c6ed4b3e70eeef52cea9042157914dd978797056
leolunac-Holbertons <2176@holbertonschool.com>
2020-10-22 00:57:57 +0000

    Write a program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,46 +1,50 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
+
 /**
-* op_add - Struct op
+* op_add - adds a and b
+* @a: first number
+* @b: second number
 *
-* @a: The operator
-* @b: The function associated
-* Return: int
+* Return: sum of a and b
 */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
-* op_sub - Struct op
+* op_sub - subtracts b from a
+* @a: first number
+* @b: second number
 *
-* @a: The operator
-* @b: The function associated
-* Return: int
+* Return: difference of a and b
 */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
-* op_mul - Struct op
+* op_mul - multiplies a and b
+* @a: first number
+* @b: second number
 *
-* @a: The operator
-* @b: The function associated
-* Return: int
+* Return: multiplication of a and b
 */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
-* op_div - Struct op
+* op_div - divides a by b
+* @a: first number
+* @b: second number
 *
-* @a: The operator
-* @b: The function associated
-* Return: int
-*/
+* Return: integer division of a by b
+        */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -50,12 +54,13 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+
 /**
-* op_mod - Struct op
+* op_mod - find the remainder of the division of a by b
+* @a: first number
+* @b: second number
 *
-* @a: The operator
-* @b: The function associated
-* Return: int
+* Return: remainder of the division of a by b
 */
 int op_mod(int a, int b)
 {
