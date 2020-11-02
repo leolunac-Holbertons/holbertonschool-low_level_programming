0452451532061d1062cfd32a2388f47394e18288
leolunac-Holbertons <2176@holbertonschool.com>
2020-11-02 19:14:58 +0000

    Write a function that prints before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+void print_fast(void) __attribute__ ((constructor));
+
+/**
+* print_fast - prints before main
+*
+* Return: void
+*/
+void print_fast(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
