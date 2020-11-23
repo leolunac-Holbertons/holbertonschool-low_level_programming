9337e9145ca769e7c64afdd673297fa4094f8982
leolunac-Holbertons <2176@holbertonschool.com>
2020-11-23 10:37:59 +0000

    Write a function that finds the loop in a linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,53 +1,27 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
+* find_listint_loop - finds a loop in a linked list
 *
-* Return: Always 0.
+* @head: linked list to search
+*
+* Return: address of node where loop starts/returns, NULL if no loop
 */
-int main(void)
+listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *head;
-	listint_t *head2;
-	listint_t *node;
+	listint_t *ptr, *end;
 
-	head2 = NULL;
-	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 1);
-	add_nodeint(&head2, 2);
-	add_nodeint(&head2, 3);
-	add_nodeint(&head2, 4);
-	add_nodeint(&head2, 98);
-	add_nodeint(&head2, 402);
-	add_nodeint(&head2, 1024);
-	print_listint_safe(head2);
-	node = find_listint_loop(head2);
-	if (node != NULL)
+	if (head == NULL)
+		return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
 	{
-		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
 	}
-	free_listint_safe(&head2);
-	head = NULL;
-	node = add_nodeint(&head, 0);
-	add_nodeint(&head, 1);
-	add_nodeint(&head, 2);
-	add_nodeint(&head, 3);
-	add_nodeint(&head, 4);
-	add_nodeint(&head, 5);
-	add_nodeint(&head, 6);
-	node->next = add_nodeint(&head, 7);
-	add_nodeint(&head, 98);
-	add_nodeint(&head, 402);
-	add_nodeint(&head, 1024);
-	print_listint_safe(head);
-	node = find_listint_loop(head);
-	if (node != NULL)
-	{
-		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
-	}
-	free_listint_safe(&head);
-	return (0);
+
+	return (NULL);
 }
