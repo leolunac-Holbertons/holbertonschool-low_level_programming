3929be2280609a41cbced84d92f64544256592ac
leolunac-Holbertons <2176@holbertonschool.com>
2020-11-23 10:29:25 +0000

    Write a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,41 +1,66 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
-* main - check the code for Holberton School students.
+* find_listint_loop_fl - finds a loop in a linked list
 *
-* Return: Always 0.
+* @head: linked list to search
+*
+* Return: address of node where loop starts/returns, NULL if no loop
 */
-int main(void)
+listint_t *find_listint_loop_fl(listint_t *head)
 {
-	listint_t *head;
-	listint_t *head2;
-	listint_t *node;
+	listint_t *ptr, *end;
 
-	head2 = NULL;
-	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 1);
-	add_nodeint(&head2, 2);
-	add_nodeint(&head2, 3);
-	add_nodeint(&head2, 4);
-	add_nodeint(&head2, 98);
-	add_nodeint(&head2, 402);
-	add_nodeint(&head2, 1024);
-	print_listint_safe(head2);
-	head = NULL;
-	node = add_nodeint(&head, 0);
-	add_nodeint(&head, 1);
-	add_nodeint(&head, 2);
-	add_nodeint(&head, 3);
-	add_nodeint(&head, 4);
-	node->next = add_nodeint(&head, 98);
-	add_nodeint(&head, 402);
-	add_nodeint(&head, 1024);
-	print_listint_safe(head);
-	free_listint_safe(&head2);
-	free_listint_safe(&head);
-	printf("%p, %p\n", (void *)head2, (void *)head);
-	return (0);
+	if (head == NULL)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
+
+/**
+* free_listint_safe - frees a listint list, even if it has a loop
+*
+* @h: head of list
+*
+* Return: number of nodes freed
+*/
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *next, *loopnode;
+	size_t len;
+	int loop = 1;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	loopnode = find_listint_loop_fl(*h);
+	for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
+	{
+		len++;
+		next = (*h)->next;
+		if (*h == loopnode && loop)
+		{
+			if (loopnode == loopnode->next)
+			{
+				free(*h);
+				break;
+			}
+			len++;
+			next = next->next;
+			free((*h)->next);
+			loop = 0;
+		}
+		free(*h);
+	}
+	*h = NULL;
+	return (len);
 }
